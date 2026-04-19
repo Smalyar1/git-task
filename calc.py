126effc6ead8fb3aa7ece8101465be5cf7942671
SMalyar <smalyar@mail.ru>
2026-04-19 22:58:27 +0300

    fix bugs and add sqrt

--- calc.py
+++ calc.py
@@ -1,17 +1,17 @@
-# Автор: ...
-
+# Автор: Маляр Сергей
+import math
 def add(a, b):
     return a + b
-
-
 def subtract(a, b):
     return a - b
-
-
 def multiply(a, b):
-    # TODO: не реализовано
-    return None
-
+    # Returns the product of two numbers
+    return a*b
+def sqrt(x):
+    """Returns the square root of a number"""
+    if x < 0:
+        raise ValueError("The square root of a negative number is not defined")
+    return math.sqrt(x)
 
 if __name__ == "__main__":
     print("Простой калькулятор запущен.")
